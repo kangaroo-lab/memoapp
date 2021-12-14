32d1e121419284a8df3d41b1f8090730af09cb0c
kengo <kobano@icloud.com>
2021-12-14 18:23:54 +0900

    Memo detail screen

--- App.jsx
+++ App.jsx
@@ -1,26 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import Hello from './src/components/Hello'
+
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello bang style={{fontSize: 16}}>small World</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable */}
-      <StatusBar style="auto" />
-    </View>
+    <MemoDetailScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {string, shape} from 'prop-types';
+
+export default function circleButton(props){
+    const {children, style} = props
+    return (
+    <View style={[styles.circleButton, style]}>
+        <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+
+    );
+}
+
+circleButton.protoTypes = {
+    children: string.isRequired,
+    style: shape(),
+};
+
+circleButton.defaultProps = {
+    style: null,
+};
+
+const styles = StyleSheet.create({
+    circleButton: {
+        backgroundColor: '#467FD3',
+        width: 64,
+        height: 64,
+        borderRadius:32,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        shadowColor: '#000000',
+        shadowOffset: {width: 0, height:8},
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        elevation: 8,
+
+      },
+      circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 40,
+      },
+
+})
