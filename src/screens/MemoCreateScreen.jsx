38c2be78ccb577501a5e7e2dcef4e42fa26350bf
kengo <kobano@icloud.com>
2021-12-16 13:49:25 +0900

    replace Avoid to Safe

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet,KeyboardAvoidingView, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen(){
     return(
-        <KeyboardAvoidingView style={styles.container} behavior='height'>
+        <KeyboardSafeView style={styles.container}>
             <AppBar/>
             <View style={styles.inputContainer}>
                 <TextInput value="" multiline style={styles.input}/>
             </View>
             <CircleButton name='check'/>
-        </KeyboardAvoidingView>
+        </KeyboardSafeView>
     );
 }
 
